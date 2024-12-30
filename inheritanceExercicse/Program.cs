bc1aa68e6dccefacf8df0ecb3ee73d5acbd9b189
simorai <109289730+simorai@users.noreply.github.com>
2024-12-30 17:10:53 +0000

    Add project files.

---
+++ inheritanceExercicse/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace inheritanceExercicse
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
